44ba9d0d86bcd9aa2589adf838037bba29438690
Dillon H <dillonrhamilton@gmail.com>
2015-12-09 01:17:20 -0500

    update api

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import request from 'superagent';
 
 class App extends React.Component {
     render() {
@@ -14,16 +15,25 @@ class App extends React.Component {
 const RecipeList = React.createClass({
     getInitialState() {
         return {
-            recipies: []
+            greeting: 'Sup'
         };
     },
     componentWillMount() {
+        request
+            .get('/api')
+            .end((err, res) => {
+                console.log('error', err);
+                console.log('res', res);
 
+                var data = JSON.parse(res.text);
+
+                this.setState({ greeting: data.greeting });
+            })
     },
     render() {
         return (
             <div>
-                Apples
+                {this.state.greeting}
             </div>
         );
     }
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+var request = require('superagent');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api', function (req, res) {
+    var data = {
+        greeting: 'bubber ducky'
+    };
+
+    // request
+    //     .get()
+    //     .end(function(err, res){
+    //
+    //     });
+
+    res.send(data);
 });
 
 app.set('port', (process.env.PORT || 5000));
